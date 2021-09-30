4bfadea184871dc873e35d60efe3e7543ba41804
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 22:58:20 -0400

    Create Program3.c

---
+++ Program3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//each char is a numeric value
+//Please replace ??? at line 6 with an integer(not a char) so that your code builds and prints out c = * 
+//Please only change ???
+int main() {
+	int ch = A;//TODO: use an int, not a char
+	printf("c = %c\n", ch);	
+	return 0;
+}
