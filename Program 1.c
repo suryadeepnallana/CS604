a16fda43363af007dda3b1f40c4a565aa22257f3
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 22:48:49 -0400

    Update and rename Program 1 to Program 1.c

--- Program 1.c
+++ Program 1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 { 
-int a,b;
+int a,b,c;
 printf("Enter two Numbers : ");
 scanf("%d %d",&a,&b);
-
+a+b=c;
 //TODO:1. declare a variable c
 //TODO:2. store a plus b into c
 printf("The Sum is %d\n",c);
