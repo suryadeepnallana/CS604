d4f18902f4191302178ccb6608a553ab2350cba4
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 23:03:38 -0400

    Create Program5.c

---
+++ Program5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	float res;
+	int i = 3;
+	int p = 2;		
+
+	res =  i / p; 
+	//TODO: this prints out 1.000000 because / is an integer division
+	//TODO: please modify the expression so the division yields a real number (1.500000 in this case) WITHOUT changing the declared type of i or p.	
+	//hint: try to make either divisor or dividend a float type
+	printf("%f\n", res);
+	return 0;
+}
