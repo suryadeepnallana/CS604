4e691f3d63fd88ca3ce3518818b493dd3e78e24f
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 23:08:31 -0400

    Create Program8.c

---
+++ Program8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    printf("please enter 2 numbers to be swapped\n");
+    scanf("%d%d", &a, &b);
+
+    printf("before:a=%d, b=%d\n",a,b);
+
+    //TODO: swap the value of a and b
+    //TODO: for instance, if a=3, b=4
+    //TODO: then after swap a=4, b=3
+    printf("after:a=%d, b=%d\n",a,b);
+    return 0;
+}
