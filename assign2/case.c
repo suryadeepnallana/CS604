4f60f1dec32c8a2c608bb21566e6e22e3784ce80
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-30 19:37:43 -0400

    Update case.c

--- assign2/case.c
+++ assign2/case.c
@@ -2,10 +2,16 @@
 #include<stdio.h>
 int main ()
 {
-    char ch;
+    char ch[100];
     int no;
     printf("Enter a lowercase character :\n");
     scanf("%c", &ch);
+    for (i=0; ch[i]!='\0';i++){
+      if(ch[i] >= 'a' && ch[i] <= 'z') {
+         s[i] = s[i] - 32;
+         s[i]=no;
+      }
+   }
     //TODO: convert lowercase character to uppercase. You are not allowed to use
     //if or switch. 
     //TODO: for instance, if ch stores char a, then char A should be stored into no
