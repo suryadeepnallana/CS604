36ef595adfff2d8dc2b9b26f822903c5662b864e
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 22:55:01 -0400

    Create Program2.c

---
+++ Program2.c
@@ -0,0 +1,18 @@
+// calculate Area and Circumference of a circle
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float r;
+    float a,c;
+    printf("enter radius :\n");
+    scanf("%f", &r);
+    a=pi*pow(r,2);
+    c=2*pi*r;
+    //r stores radius 
+    //TODO: 1. declare a variable a to hold area and a variable c to hold circumference
+    //TODO  2. calculate and store area into a and circumference into c (you can assume pi = 3.14)
+    printf("Area = %f circumference = %f\n",a,c);
+    return 0;
+}
