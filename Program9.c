a5e19a191ca524df199bc5e7cca8842a74d3edf8
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-29 23:09:44 -0400

    Create Program9.c

---
+++ Program9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{	//TODO: replace ??? to declare the right type for c and f
+	??? c, f;
+	printf("Input Temp in Fahrenheit: ");
+	scanf("%f", &f);
+	//TODO: convert fahrenheit to celsius. the formula is
+	//TODO: c = (f - 32) * 5/9;
+	//TODO: replace ??? to print out the celsius with two digits to the right of decimal point
+	printf("\nTemp in Celsius is: ???\n", c);
+	return 0;
+}
