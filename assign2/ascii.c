42e3831ef521f98d938fbe349b0df1f5e20607e4
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-30 19:16:08 -0400

    Update ascii.c

--- assign2/ascii.c
+++ assign2/ascii.c
@@ -3,7 +3,7 @@
 //Please replace ??? at line 6 with an integer(not a char) so that your code builds and prints out c = * 
 //Please only change ???
 int main() {
-	int ch = ???;//TODO: use an int, not a char
+	int ch = c;//TODO: use an int, not a char
 	printf("c = %c\n", ch);	
 	return 0;
 }
