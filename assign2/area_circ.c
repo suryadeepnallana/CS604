99815748084a05f9a8c6987bef6c1f6313928337
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-30 19:15:04 -0400

    Update area_circ.c

--- assign2/area_circ.c
+++ assign2/area_circ.c
@@ -4,8 +4,11 @@
 int main()
 {
     float r;
+    float a,c;
     printf("enter radius :\n");
     scanf("%f", &r);
+    a=pi*pow(r,2);
+    c=2*pi*r;
     //r stores radius 
     //TODO: 1. declare a variable a to hold area and a variable c to hold circumference
     //TODO  2. calculate and store area into a and circumference into c (you can assume pi = 3.14)
