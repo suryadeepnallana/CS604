2a3643c3ffef0e99708bb8f2f0f408ecdac8789d
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-09-30 19:39:38 -0400

    Update division.c

--- assign2/division.c
+++ assign2/division.c
@@ -6,7 +6,7 @@ int main() {
 	int i = 3;
 	int p = 2;		
 
-	res =  i / p; 
+	res =  float i / p; 
 	//TODO: this prints out 1.000000 because / is an integer division
 	//TODO: please modify the expression so the division yields a real number (1.500000 in this case) WITHOUT changing the declared type of i or p.	
 	//hint: try to make either divisor or dividend a float type
