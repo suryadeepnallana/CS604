4e71d26d1ccfb179eb26cee3fe483e007b8774b7
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-10-01 16:19:35 -0400

    Update grades.c

--- assign2/grades.c
+++ assign2/grades.c
@@ -9,6 +9,7 @@ int main()
     float total,average;
     printf("please enter grades of 5 subjects : \n");
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    total=5;
     average=a+b+c+d+e/5;
     //TODO calculate total and average of a student's grade
     //TODO again beware of integer division. If you enter 1,2,2,2,2 then average is expected to be 1.8 instead of 1 but integer division will yield 1.
