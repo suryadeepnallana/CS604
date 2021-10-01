dc57316a7acadedc26f65774537997f5f7d317cc
suryadeepnallana <64959092+suryadeepnallana@users.noreply.github.com>
2021-10-01 17:02:21 -0400

    Update case.c

--- assign2/case.c
+++ assign2/case.c
@@ -1,22 +1,17 @@
-//No toupper() or any other C function.
+////No toupper() or any other C function.
 #include<stdio.h>
 int main ()
 {
-    char ch[100];
+    char ch;
     int no;
+    int ascii;
     printf("Enter a lowercase character :\n");
     scanf("%c", &ch);
-    for (i=0; ch[i]!='\0';i++){
-      if(ch[i] >= 'a' && ch[i] <= 'z') {
-         s[i] = s[i] - 32;
-         s[i]=no;
-      }
-   }
+    ascii=ch;
+    no=ascii-32;
     //TODO: convert lowercase character to uppercase. You are not allowed to use
     //if or switch. 
     //TODO: for instance, if ch stores char a, then char A should be stored into no
     printf("Letter in capital  : %c\n", no);
     return 0;
-
-
 }
